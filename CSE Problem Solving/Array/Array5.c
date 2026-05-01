c6f07dddc1eb31ed7ed590f22524bdd32fe163b0
jubaerhimel <jubaer.himell@gmail.com>
2026-05-02 02:44:49 +0600

    Modify program to count even and odd numbers
    Updated the program to count even and odd numbers in an array instead of attendance.

--- CSE Problem Solving/Array/Array5.c
+++ CSE Problem Solving/Array/Array5.c
@@ -1,37 +1,26 @@
-/* Tasks:
-Total present
-Longest consecutive present */
+//Count how many even and odd numbers are in array.
 
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, i = 0;
+    int arr[100];
+    int even = 0, odd = 0;
 
-    printf("Enter number of days: ");
+    printf("Enter size: ");
     scanf("%d", &n);
 
-    int attendance[n];
+    while (i < n) {
+        scanf("%d", &arr[i]);
 
-    printf("Enter attendance (1/0):\n");
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &attendance[i]);
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+
+        i++;
     }
 
-    int present = 0;
-    int max = 0, current = 0;
-
-    for(int i = 0; i < n; i++) {
-        if(attendance[i] == 1) {
-            present++;
-            current++;
-            if(current > max)
-                max = current;
-        } else {
-            current = 0;
-        }
-    }
-
-    printf("Present: %d\n", present);
-    printf("Longest Streak: %d\n", max);
+    printf("Even = %d, Odd = %d\n", even, odd);
 
 }
