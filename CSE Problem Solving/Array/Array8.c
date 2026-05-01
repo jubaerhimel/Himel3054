8e55018484aeeab68a830a9370f7d3bd647c5902
jubaerhimel <jubaer.himell@gmail.com>
2026-05-01 23:04:24 +0600

    Array8.c
    Implement a program to find the maximum and minimum values in an array.

---
+++ CSE Problem Solving/Array/Array8.c
@@ -0,0 +1,30 @@
+//Maximum and Minimum 
+
+#include<stdio.h>
+int main (){
+
+    int n, sum ;
+
+    printf (" How many numbers do you want ? ");
+    scanf ("%d",&n);
+
+    int arr[n];
+    for (int i=0; i<n; i++){
+        printf ("enter the numbers :");
+        scanf ("%d",&arr[i]); }
+
+        int max = arr[0] , min = arr[0];
+
+         for (int i=1; i<n; i++){
+          if (max < arr [i]){
+          max = arr [i]; }
+        
+          if (min > arr [i]){
+          min = arr [i]; }
+          }
+
+printf ("Max Number = %d And \n Min Number = %d",max,min);
+
+
+ 
+}
